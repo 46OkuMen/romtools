3373fb4d5e2f3b6da0beed9ca98b1a1f67a179ac
Max Silbiger <max.silbiger@gmail.com>
2017-01-19 05:33:10 -0500

    Correct insert() syntax, support paths within the disk

--- disk.py
+++ disk.py
@@ -8,39 +8,57 @@ http://euee.web.fc2.com/tool/nd.html
 """
 
 import os
+# TODO: Use subprocess instead of os.system to check the output and see if it worked.
 
 class Disk:
-	def __init__(self, filename):
-		self.filename = filename
-		self.extension = filename.split('.')[-1]
-		self.path = os.path.abspath(os.path.join(filename, os.pardir))
+    def __init__(self, filename):
+        self.filename = filename
+        self.extension = filename.split('.')[-1]
+        self.path = os.path.abspath(os.path.join(filename, os.pardir))
 
 
-	def extract(self, filename):
-		cmd = 'ndc G %s 0 %s .' % (diskname, filename)
-		os.system(cmd)
+    def extract(self, filename):
+        cmd = 'ndc G %s 0 %s .' % (self.filename, filename)
+        print cmd
+        os.system(cmd)
 
-	def insert(self, filename):
-		cmd = 'ndc P %s 0 %s .' % (diskname, filename)
-		os.system(cmd)
+    def insert(self, filename, path_in_disk=None):
+        del_cmd = 'ndc D %s 0' % (self.filename)
+        if path_in_disk:
+            del_cmd += ' ' + os.path.join(path_in_disk, filename)
+        else:
+            del_cmd += ' ' + filename
+        os.system(del_cmd)
+
+        cmd = 'ndc P %s 0 %s' % (self.filename, filename)
+        if path_in_disk:
+            cmd += ' ' + path_in_disk
+        print cmd
+        os.system(cmd)
 
 """
 kuoushi's note:
-	Commands for using NDC to export/import/delete files:
-	ndc G imgname.ext partition [pathtofileinimg]  exportfolder
-	This command exports all or some files from the image. If you want to extract a certain file from an image into the folder where ndc is located you would use this command, for example: ndc G img.hdi 0 certainfolder\file.txt .
-	The last . is important, it tells ndc to put the file in the same location
+    Commands for using NDC to export/import/delete files:
+    ndc G imgname.ext partition [pathtofileinimg]  exportfolder
+    This command exports all or some files from the image. If you want to extract a certain file from an image 
+    into the folder where ndc is located you would use this command, for example: 
 
-	ndc P imgname.ext partition filetoimport destination
-	To put the same file  from above back into the place you found it, you'd do something like the following: ndc P img.hdi 0 file.txt certainfolder
+    ndc G img.hdi 0 certainfolder\file.txt .
 
-	Delete is similar: ndc D img.hdi 0 filetodelete
+    The last . is important, it tells ndc to put the file in the same location
 
-	Supported filetypes listed here:
-	http://euee.web.fc2.com/tool/nd.html
+    ndc P imgname.ext partition filetoimport destination
+
+    To put the same file  from above back into the place you found it, you'd do something like the following: 
+    ndc P img.hdi 0 file.txt certainfolder
+
+    Delete is similar: ndc D img.hdi 0 filetodelete
+
+    Supported filetypes listed here:
+    http://euee.web.fc2.com/tool/nd.html
 
 """
 
 if __name__ == '__main__':
-	EVODisk = Disk('46OM.hdi')
-	EVODisk.insert('windhex.cfg')
+    EVODisk = Disk('46OM.hdi')
+    EVODisk.insert('windhex.cfg')
