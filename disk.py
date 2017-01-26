9291be12e140edf5a4fb7a4285d62ba49aedbacf
Max Silbiger <max.silbiger@gmail.com>
2017-01-26 16:26:39 -0500

    Add basic classes from EVO

--- disk.py
+++ disk.py
@@ -7,7 +7,6 @@ http://euee.web.fc2.com/tool/nd.html
 
 """
 
-
 import os
 # TODO: Use subprocess instead of os.system to check the output and see if it worked.
 from shutil import copyfile
@@ -19,6 +18,15 @@ SUPPORTED_FILE_FORMATS = ['fdi', 'hdi', 'hdm', 'flp', 'vmdk', 'dsk', 'vfd', 'vhd
                           'h2', 'h3', 'h4', 'hdm', 'slh']
 # (hdm requires conversion to flp, but that conersion gets done below)
 
+def file_to_string(file_path, start=0, length=0):
+    # Defaults: read full file from start.
+    f = open(file_path, 'rb+')
+    f.seek(start)
+    if length:
+        return f.read(length)
+    else:
+        return f.read()
+
 class Disk:
     def __init__(self, filename):
         self.filename = filename
@@ -26,7 +34,7 @@ class Disk:
         assert self.extension in SUPPORTED_FILE_FORMATS # TODO use an exception
 
         self.original_extension = self.extension
-        self.abspath = os.path.abspath(os.path.join(filename, os.pardir))
+        self.dir = os.path.abspath(os.path.join(filename, os.pardir))
 
         if self.extension == 'hdm':
             new_disk_filename = self.filename.split('.')[0] + '.flp'
@@ -39,6 +47,8 @@ class Disk:
 
         cmd = 'ndc G %s 0 %s .' % (self.filename, filename)
         os.system(cmd)
+
+        return Gamefile(self, filename)
 
     def delete(self, filename, path_in_disk=None):
         del_cmd = 'ndc D %s 0' % (self.filename)
@@ -62,28 +72,59 @@ class Disk:
             original_disk_filename = self.filename.split('.')[0] + '.hdm'
             copyfile(self.filename, original_disk_filename)
 
-"""
-kuoushi's note:
-    Commands for using NDC to export/import/delete files:
-    ndc G imgname.ext partition [pathtofileinimg]  exportfolder
-    This command exports all or some files from the image. If you want to extract a certain file from an image 
-    into the folder where ndc is located you would use this command, for example: 
+    def __repr__(self):
+        return self.filename
 
-    ndc G img.hdi 0 certainfolder\file.txt .
+class Gamefile(object):
+    def __init__(self, disk, dest_disk, filename):
+        self.filename = filename
+        self.disk = disk
+        self.dest_disk = dest_disk
 
-    The last . is important, it tells ndc to put the file in the same location
+        self.original_filestring = file_to_string(filename)
+        self.filestring = "" + self.original_filestring
 
-    ndc P imgname.ext partition filetoimport destination
+    #def incorporate(self):
+    #    """Add the edited file to the Disk in the original's place."""
+    #    for b in self.blocks:
+    #        b.incorporate()
 
-    To put the same file  from above back into the place you found it, you'd do something like the following: 
-    ndc P img.hdi 0 file.txt certainfolder
+    def write(self):
+        """Write the new data to an independent file for later inspection."""
+        dest_path = os.path.join(self.dest_disk.dir, self.filename)
 
-    Delete is similar: ndc D img.hdi 0 filetodelete
+        with open(dest_path, 'wb') as fileopen:
+            fileopen.write(self.filestring)
 
-    Supported filetypes listed here:
-    http://euee.web.fc2.com/tool/nd.html
+        self.dest_disk.insert(self.filename)
 
-"""
+
+    def __repr__(self):
+        return self.filename
+
+class Block(object):
+    """A text block.
+
+    Attributes:
+        gamefile: The EXEFile or DATFile object it belongs to.
+        start = Beginning offset of the block.
+        stop  = Ending offset of the block.
+        original_blockstring: Hex string of entire block.
+        blockstring: Hex string of entire block for editing.
+        translations: List of Translation objects.
+        """
+
+    def __init__(self, gamefile, (start, stop)):
+        self.gamefile = gamefile
+        self.start = start
+        self.stop = stop
+
+        self.original_blockstring = file_to_string(self.gamefile.disk.filename)
+        self.blockstring = "" + self.original_blockstring
+
+    def __repr__(self):
+        return "%s (%s, %s)" % (self.gamefile, hex(self.start), hex(self.stop))
+
 
 if __name__ == '__main__':
     #EVODisk = Disk('46OM.hdi')
@@ -92,3 +133,4 @@ if __name__ == '__main__':
     EVOHDM = Disk('EVO.hdm')
     EVOHDM.insert('AV300.GDT')
     EVOHDM.extract('AV300.GDT')
+
---
+++ dump.py
@@ -0,0 +1,93 @@
+from openpyxl import Workbook, load_workbook
+
+def ascii_to_hex_string(eng, control_codes={}):
+    """Returns a hex string of the ascii bytes of a given english (translated) string."""
+    eng_bytestring = ""
+    if not eng:
+        return ""
+    else:
+        try:
+            eng = str(eng)
+        except UnicodeEncodeError:
+            # Tried to encode a fullwidth number. Encode it as sjis instead.
+            eng = eng.encode('shift-jis')
+        #for char in eng:
+        #   eng_bytestring += "\x" + "%02x" % ord(char)
+
+        eng_bytestring = eng
+
+        for cc in control_codes:
+            cc_hex = ascii_to_hex_string(cc)
+            if cc_hex in eng_bytestring:
+                eng_bytestring = eng_bytestring.replace(cc_hex, control_codes[cc])
+
+        return eng_bytestring
+
+def sjis_to_hex_string(jp, control_codes={}):
+    """Returns a hex string of the Shift JIS bytes of a given japanese string."""
+    jp_bytestring = ""
+    try:
+        sjis = jp.encode('shift-jis')
+    except AttributeError:
+        # Trying to encode numbers throws an attribute error; they aren't important, so just keep the number
+        sjis = str(jp)
+        
+    jp_bytestring = sjis
+
+    for cc in control_codes:
+        cc_hex = sjis_to_hex_string(cc)
+        if cc_hex in jp_bytestring:
+            jp_bytestring = jp_bytestring.replace(cc_hex, control_codes[cc])
+
+    return jp_bytestring
+
+class Translation(object):
+    """Has an offset, a SJIS japanese string, and an ASCII english string."""
+    def __init__(self, block, location, japanese, english):
+        self.location = location
+        self.block = block
+        self.japanese = japanese
+        self.english = english
+
+        self.jp_bytestring = sjis_to_hex_string(japanese)
+        self.en_bytestring = ascii_to_hex_string(english)
+
+    def __repr__(self):
+        return self.english
+
+
+class DumpExcel(object):
+    """
+    Takes a dump excel path, and lets you get a block's translations from it.
+    """
+    def __init__(self, path, control_codes={}):
+        self.path = path
+        self.workbook = load_workbook(self.path)
+        self.control_codes = control_codes
+
+    def get_translations(self, block):
+        """Get the translations for a file."""
+        # So they can make use of Translation() objects as well.
+        trans = []    # translations[offset] = Translation()
+        worksheet = self.workbook.get_sheet_by_name(block.gamefile.filename)
+
+        for row in worksheet.rows[1:]:  # Skip the first row, it's just labels
+            try:
+                offset = int(row[0].value, 16)
+            except TypeError:
+                # Either a blank line or a total value. Ignore it.
+                break
+            if block.start <= offset < block.stop:
+                japanese = row[1].value
+                english = row[3].value
+
+                #if isinstance(japanese, long):
+                #    # Causes some encoding problems? Trying to skip them for now
+                #    continue
+
+                # Yeah this is important - blank strings are None (non-iterable), so use "" instead.
+                if not english:
+                    english = ""
+
+                trans.append(Translation(block, offset, japanese, english))
+        return trans
